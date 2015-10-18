d435c71c5106024a0bd2f0a1e49bc8a1da244a81
alan <alan_araujo_sousa@hotmail.com>
2015-10-18 18:37:29 -0200

    [WEB] Increases accuracy of dynamic load of maps.

--- rsrc/js/main.js
+++ rsrc/js/main.js
@@ -4,6 +4,7 @@ dataBase.units = [];
 
 terrains = [];
 world = new Array(2500);
+poolOfDownloadDataChunck = new Array();
 map = new Object();
 paper = new Object();
 panZoom = new Object();
@@ -153,75 +154,105 @@ $(document).ready(function () {
     };
 
     function drawLocalMapArround(initX, initY) {
+        var info = getInfoWorldDataChunck(initX, initY);
+        var index = info.index;
+        var deltaX = info.deltaX;
+        var deltaY = info.deltaY;
 
+        if (world[index] == null && !(index in poolOfDownloadDataChunck)) {
+            console.log("Loading map chunck: " + index);
+            poolOfDownloadDataChunck.push(index);
+            var promise = retriveWorldDataChunck(index);
+            promise.always(function (data) {
+                var cont = poolOfDownloadDataChunck.indexOf(index);
+                poolOfDownloadDataChunck.splice(cont, 1);
+                world[index] = data;
+
+                var isPainted = d3.select("#map" + index)[0][0] != null;
+                if (!isPainted) { // if this chunck is already painted.
+                    var group = d3.select('#mapGroup').append("g").attr("id", "map" + index);
+                    for (var x = 1; x <= 100; x++) {
+                        var factorX = x + deltaX;
+                        for (var y = 1; y <= 100; y++) {
+                            var factorY = y + deltaY;
+                            group.append("rect")
+                                .attr("id", "x" + factorX + "y" + factorY)
+                                .attr("x", factorX * gridCellSize)
+                                .attr("y", factorY * gridCellSize)
+                                .attr("width", gridCellSize)
+                                .attr("height", gridCellSize)
+                                .style("stroke-width", "1px")
+                                .style("stroke", "black");
+                        }
+                    }
+                } else {
+                    console.warn("Tried to duplicate on screen the map: " + index);
+                }
+            });
+        }
+    };
+
+    // this is for test
+    function getInfoWorldDataChunck(initX, initY) {
         var deltaX = initX;
         var deltaY = initY;
         if (deltaX % 100 == 0)
             deltaX--;
         if (deltaY % 100 == 0)
             deltaY--;
-        deltaX = deltaX / 100;
-        deltaY = deltaY / 100;
-        deltaX = Number.parseInt(deltaX);
-        deltaY = Number.parseInt(deltaY);
-        var index = 0;
+        deltaX = Number.parseInt(deltaX / 100);
+        deltaY = Number.parseInt(deltaY / 100);
+        var index = 1;
         index += deltaX * 50;
         index += deltaY;
-        index++;
         deltaX *= 100;
         deltaY *= 100;
 
-        var promise = retriveWorldDataChunck(index);
-        promise.always(function (data) {
-            world[index] = data;
-            debugger;
-
-            for (var x = 1; x <= 100; x++) {
-                var factorX = x + deltaX;
-                for (var y = 1; y <= 100; y++) {
-                    var factorY = y + deltaY;
-                    d3.select('#mapGroup').append("rect")
-                        .attr("id", factorX + ":" + factorY)
-                        .attr("x", factorX * gridCellSize)
-                        .attr("y", factorY * gridCellSize)
-                        .attr("width", gridCellSize)
-                        .attr("height", gridCellSize)
-                        .style("stroke-width", "1px")
-                        .style("stroke", "black");
-                }
-            }
-        });
-    };
-
-    function getIndexWorldDataChunck(initX, initY) {
-        var deltaX = initX;
-        var deltaY = initY;
-        if (deltaX % 100 == 0)
-            deltaX--;
-        if (deltaY % 100 == 0)
-            deltaY--;
-        deltaX = deltaX / 100;
-        deltaY = deltaY / 100;
-        deltaX = Number.parseInt(deltaX);
-        deltaY = Number.parseInt(deltaY);
-        var index = 0;
-        index += deltaX * 50;
-        index += deltaY;
-        index++;
-        return index;
+        var info = new Object();
+        info.index = index;
+        info.deltaX = deltaX;
+        info.deltaY = deltaY;
+        return info;
     };
 
     function findTerrain(code) {
         // some calc;
-        return terrains[0];
+        return terrains[12];
     };
+
+    function stalkEmptyTiles() {
+        setInterval(
+            function () {
+                var x = panZoom.getPan().x;
+                var y = panZoom.getPan().y;
+                x = Number.parseInt(x / 100);
+                y = Number.parseInt(y / 100);
+
+                // Only calc coordinates positives.
+                if (x < 0)
+                    x *= -1;
+
+                if (y < 0)
+                    y *= -1;
+
+                drawLocalMapArround(x, y + 50);
+                drawLocalMapArround(x, y - 50);
+                drawLocalMapArround(x + 50, y);
+                drawLocalMapArround(x + 50, y + 50);
+                drawLocalMapArround(x + 50, y - 50);
+                drawLocalMapArround(x - 50, y);
+                drawLocalMapArround(x - 50, y - 50);
+                drawLocalMapArround(x - 50, y - 50);
+
+            }, 1000);
+    }
 
     function generateInitialMap() {
 
         // Obtain tiles of terrains.
-        var tileWidth = 100;
-        var tileHeight = 78;
-        var ctx = $('<canvas>').appendTo('body')[0].getContext("2d");
+        var tileWidth = 32;
+        var tileHeight = 32;
+        var ctx = $('<canvas>').css("display", "none").appendTo('body')[0].getContext("2d");
         var terrainSpriteSheet = new Image();
         terrainSpriteSheet.onload = function () {
             var imageWidth = terrainSpriteSheet.width;
@@ -249,9 +280,15 @@ $(document).ready(function () {
             });
 
             drawLocalMapArround(initX, initY);
-            // pan to specific point
 
+            // pan to specific point
+            panZoom.pan({
+                x: initX * gridCellSize * -1,
+                y: initY * gridCellSize * -1
+            });
+
+            stalkEmptyTiles();
         }
-        terrainSpriteSheet.src = "../img/sprites/map/map1.jpg";
+        terrainSpriteSheet.src = "../img/sprites/map/tileset.png";
     };
 });
