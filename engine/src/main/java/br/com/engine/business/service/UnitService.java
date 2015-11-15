73e05e249d0765509bf5c170b83853a78b8b46b8
alanAraujo <alan_araujo_sousa@hotmail.com>
2015-11-15 00:29:43 -0200

    [ENGINE] On movement save actual place in cache.

--- engine/src/main/java/br/com/engine/business/service/UnitService.java
+++ engine/src/main/java/br/com/engine/business/service/UnitService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.commons.exceptions.InvalidArgumentException;
 import br.com.commons.exceptions.MovementException;
+import br.com.commons.transport.MovementObject;
 import br.com.commons.transport.PlaceObject;
 import br.com.commons.transport.UnitObject;
 import br.com.commons.transport.UserObject;
@@ -23,45 +24,48 @@ public class UnitService {
 
 	@Autowired
 	private UserDAO userDAO;
-	
+
 	@Autowired
 	private UnitDAO unitDAO;
 
 	public void move(UserObject user, Long id, Deque<Integer> places) {
-		
+
 		Unit unit = this.unitDAO.findByIdAndUser(id, user.getId());
-		if (unit == null) 
+		if (unit == null)
 			throw new InvalidArgumentException("Unit not found: " + id);
 
 		UnitObject unitObject = UnitCache.getInstance().findUnitByIdOnMove(id);
 		PlaceObject placeObject = unitObject.getPlace();
+		Integer actualX = placeObject.getX();
+		Integer actualY = placeObject.getY();
 
 		boolean isValid = Utils.validateMovement(placeObject, places);
-		if (!isValid) 
+		if (!isValid)
 			throw new MovementException("You choose a movement invalid!");
-		
+
 		Iterator<Integer> iterator = places.iterator();
-		Integer x = iterator.next();
-		Integer y = iterator.next();
+		Integer nextX = iterator.next();
+		Integer nextY = iterator.next();
 
 		Integer moveBuff = Utils.calcMoveBuff(unitObject.getType(),
 				placeObject.getType());
 		Integer moveTime = Utils.DEFAULT_MOVE_TIME;
-		
-		if (x != unitObject.getPlace().getX()
-				&& y != unitObject.getPlace().getY()) {
+
+		// diagonal moviment
+		if (!nextX.equals(actualX) && !nextY.equals(actualY))
 			moveTime = Utils.DIAGONAL_MOVE_TIME;
-		}
-		
+
 		moveTime -= (moveTime * unitObject.getType().getVelocity()) / 100;
 		moveTime -= (moveTime * moveBuff) / 100;
-		
+
 		Date now = new Date();
 		now.setTime(now.getTime() + moveTime);
 		unitObject.setTimeToNextMove(now);
-		unitObject.getMovementObject().setMoves(places);
+		MovementObject movementObject = new MovementObject();
+		movementObject.setMoves(places);
+		unitObject.setMovementObject(movementObject);
 	}
-	
+
 	public List<UnitObject> listByUser(UserObject userObject) {
 		List<Unit> units = this.unitDAO.listByUser(userObject.getId());
 		List<UnitObject> unitObjects = new ArrayList<>();
